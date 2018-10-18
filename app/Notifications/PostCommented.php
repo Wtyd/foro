9adef6ebbe320f0d813f9e33c7917621d6f4cdfc
Jorge Lilao <jorgelilaoc@gmail.com>
2018-10-18 19:01:16 +0200

    Refinado el envio de notificaciones de los Posts

--- app/Notifications/PostCommented.php
+++ app/Notifications/PostCommented.php
@@ -6,16 +6,12 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
-use App\{Comment, User};
+use App\{Comment};
 
 class PostCommented extends Notification
 {
     use Queueable;
 
-    /**
-     * @var \App\User
-     */
-    public $commentAuthor;
     /**
      * @var \App\Comment
      */
@@ -26,9 +22,8 @@ class PostCommented extends Notification
      *
      * @return void
      */
-    public function __construct(User $commentAuthor, Comment $comment)
+    public function __construct(Comment $comment)
     {
-        $this->commentAuthor = $commentAuthor;
         $this->comment = $comment;
     }
 
@@ -52,9 +47,9 @@ class PostCommented extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', 'https://laravel.com')
-                    ->line('Thank you for using our application!');
+                    ->subject('Nuevo comentario en: '.$this->comment->post->title)
+                    ->line($this->comment->user->name.' escribió un comentario en: '.$this->comment->post->title)
+                    ->action('Ver post', $this->comment->post->url);
     }
 
     /**
---
+++ tests/unit/PostcommentedTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Notifications\PostCommented;
+use Illuminate\Notifications\Messages\MailMessage;
+use App\{Comment, Post, User};
+
+class PostcommentedTest extends TestCase
+{
+    /**
+     * @test
+     */
+    function it_builds_a_mail_message()
+    {
+        $post = new Post([
+            'title' => 'Título del post'
+        ]);
+
+        $author = new User([
+            'name' => 'Jorge'
+        ]);
+
+        $comment = new Comment();
+        $comment->post = $post;
+        $comment->user = $author;
+
+        $notification = new PostCommented($comment);
+
+        $subscriber = new User();
+
+        $message = $notification->toMail($subscriber);
+
+        $this->assertInstanceOf(MailMessage::class, $message);
+
+        $this->assertSame(
+            'Nuevo comentario en: Título del post',
+            $message->subject
+        );
+
+        $this->assertSame(
+            'Jorge escribió un comentario en: Título del post',
+            $message->introLines[0]
+        );
+
+        $this->assertSame($comment->post->url, $message->actionUrl);
+    }
+}
