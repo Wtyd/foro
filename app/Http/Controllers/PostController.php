37327879978cc4c6098ca2011d34769eb977ba56
Jorge Lilao <jorgelilaoc@gmail.com>
2018-10-28 20:03:09 +0100

    Listar los Posts eligiendo el orden

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,11 +9,17 @@ class PostController extends Controller
 {
     public function index(Category $category = null, Request $request)
     {
-        $posts = Post::orderBy('created_at', 'DESC')
-            ->category($category)
-            ->scopes($this->getListScopes($category, $request))
+        $routeName = $request->route()->getName();
+
+        list($orderColumn, $orderDirection) = $this->getListOrder($request->get('orden'));
+
+        $posts = Post::query()
+            ->scopes($this->getListScopes($category, $routeName))
+            ->orderBy($orderColumn, $orderDirection)
             ->paginate();
 
+        $posts->appends(request()->intersect(['orden']));
+        
         $categoryItems = $this->getCategoryItems();
 
         return view('posts.index', compact('posts', 'category', 'categoryItems'));
@@ -38,15 +44,13 @@ class PostController extends Controller
         })->toArray();
     }
 
-    protected function getListScopes(Category $category, Request $request)
+    protected function getListScopes(Category $category, string $routeName)
     {
         $scopes = [];
 
         if ($category->exists) {
             $scopes['category'] = [$category];
         }
-
-        $routeName = $request->route()->getName();
         
         if ($routeName == 'posts.pending') {
             $scopes[] = 'pending';
@@ -57,5 +61,18 @@ class PostController extends Controller
         }
 
         return $scopes;
+    }
+
+    protected function getListOrder($order)
+    {
+        if ($order == 'recientes') {
+            return ['created_at', 'desc'];
+        }
+
+        if ($order == 'antiguos') {
+            return ['created_at', 'asc'];
+        }
+
+        return ['created_at', 'desc'];
     }
 }
